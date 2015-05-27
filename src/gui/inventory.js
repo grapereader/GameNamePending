5c65b9a460c56286399f0f8e2868479751fb2372
WinMac32 <seth@viaware.ca>
2015-05-26 20:01:47 -0600

    Added an item tooltip to the inventory

--- src/gui/inventory.js
+++ src/gui/inventory.js
@@ -1,15 +1,18 @@
 define(function() {
 
     var InventoryItem = Class({
-        constructor: function(gameManager, item, tileX, tileY) {
+        constructor: function(gameManager, item, invId, tileX, tileY) {
             this.gameManager = gameManager;
+
+            this.item = item;
+            this.invId = invId;
 
             this.sprite = new PIXI.Sprite(PIXI.utils.TextureCache[item.framesNamespace]["inventory"]);
             this.sprite.scale.x = 2;
             this.sprite.scale.y = 2;
 
-            this.targetX = tileX;
-            this.targetY = tileY;
+            this.tileX = tileX;
+            this.tileY = tileY;
 
             this.sprite.x = tileX * 32;
             this.sprite.y = tileY * 32;
@@ -18,15 +21,15 @@ define(function() {
             //Similar transition as the View's on item move
             var period = this.gameManager.game.deltaTime / 17;
 
-            var dx = (this.targetX * 32 - this.sprite.x) * period;
-            var dy = (this.targetY * 32 - this.sprite.y) * period;
+            var dx = (this.tileX * 32 - this.sprite.x) * period;
+            var dy = (this.tileY * 32 - this.sprite.y) * period;
 
             this.sprite.x += dx / 20;
             this.sprite.y += dy / 20;
         },
         moveTo: function(tileX, tileY) {
-            this.targetX = tileX;
-            this.targetY = tileY;
+            this.tileX = tileX;
+            this.tileY = tileY;
         }
     });
 
@@ -45,13 +48,47 @@ define(function() {
                 if (inventory.items[i] === false) continue;
                 var x = i % 4;
                 var y = Math.floor(i / 4);
-                var item = new InventoryItem(gameManager, inventory.items[i], 0, 0);
+                var item = new InventoryItem(gameManager, inventory.items[i], i, 0, 0);
                 this.container.addChild(item.sprite);
                 this.items.push(item);
                 //Move to instead of set so we get a swaggin animnation on open.
                 //This could be really bad for functionality. :P We shall see
                 item.moveTo(x, y);
             }
+
+            var tooltip = new PIXI.Text("Test", {font: "Arial 14px"});
+            tooltip.visible = false;
+            this.container.addChild(tooltip);
+            var overlay = new PIXI.Graphics();
+            this.container.addChild(overlay);
+
+            var self = this;
+            this.container.interactive = true;
+            var over = false;
+            this.container.on("mouseover", function(e) {
+                tooltip.visible = true;
+            })
+            this.container.on("mouseout", function(e) {
+                tooltip.visible = false;
+            })
+            this.container.on("mousemove", function(e) {
+                var pos = e.data.getLocalPosition(self.container);
+                var mx = pos.x + 15;
+                var my = pos.y + 15;
+                tooltip.x = mx;
+                tooltip.y = my;
+                var item = self.getItemAt(mx, my);
+                if (item !== false) tooltip.text = item.item.name;
+            });
+        },
+        getItemAt: function(x, y) {
+            var tileX = Math.floor(x / 32);
+            var tileY = Math.floor(y / 32);
+            for (var i = 0; i < this.items.length; i++) {
+                var item = this.items[i];
+                if (item.tileX === tileX && item.tileY === tileY) return item;
+            }
+            return false;
         },
         update: function() {
             for (var i = 0; i < this.items.length; i++) {
