1409bc6aee45d6df1d4d9c5d1147fd2cb00a1b63
WinMac32 <seth@viaware.ca>
2015-05-26 18:48:34 -0600

    Just realised that this randomness doesn't even work, since affixes need to be static.

--- src/item/affix.js
+++ src/item/affix.js
@@ -12,13 +12,9 @@ define(function() {
 
             for (b in this.buffs) {
                 var buff = this.buffs[b];
-                if (buff.val.min !== undefined && buff.val.max !== undefined) {
-                    var min = buff.val.min;
-                    var max = buff.val.max;
-                    buff.val = min + Math.floor(Math.random() * ((max - min) + 1));
-                }
                 this.description = this.description.replace("!{" + b + "}", buff.val + (buff.type === "%" ? "%" : ""));
             }
+
             //console.log("Created affix " + this.description);
         }
     });
--- src/item/affixes.js
+++ src/item/affixes.js
@@ -9,10 +9,7 @@ define(["item/affix"], function(Affix) {
         new Affix(1, Affix.Type.WEAPON, "Bonus Attack Speed", "This is a test affix, bonus !{attackSpeed} attack speed", 5, {}, {
             attackSpeed: {
                 type: "%",
-                val: { //I figure we can reduce work by making some things random.
-                    min: 2,
-                    max: 5
-                }
+                val: 3
             }
         }),
         new Affix(2, Affix.Type.WEAPON, "Damage Player", "This is a test affix, damages player on activate", 5, {
