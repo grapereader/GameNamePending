47df1f48e2ac257ba123bdefd2e62f34675096cd
WinMac32 <seth@viaware.ca>
2015-06-01 16:25:01 -0600

    Added format batch job

---
+++ format.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+#Formats all code using js-beautify to enforce a standard style
+
+from subprocess import call
+import os
+
+include = {
+    "dir": ["src"]
+}
+
+exclude = {
+    "dir": ["lib", "pixi"],
+    "file": ["require.js"]
+}
+
+def is_exclude(root, file):
+    for d in include["dir"]:
+        if d not in root:
+            return True
+    for d in exclude["dir"]:
+        if d in root:
+            return True
+    for f in exclude["file"]:
+        if f in file:
+            return True
+    return False
+
+formatted = 0
+
+for root, dirs, files in os.walk("."):
+    for file in files:
+        if is_exclude(root, file):
+            continue;
+        path = os.path.join(root, file)
+        call("python ./format/js-beautify -r " + path, shell=True)
+        formatted += 1
