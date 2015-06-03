b5352db81f5fd1e364b839321be830ae4f9fd6f3
WinMac32 <seth@viaware.ca>
2015-06-02 21:22:29 -0600

    Move PixiJS' normal map filter into our codebase so it can be modified.

---
+++ src/lighting/filter.js
@@ -0,0 +1,36 @@
+define(["lighting/fragshader", function(FragmentShader) {
+
+    var LightingFilter = Class({
+        constructor: function(texture) {
+            this.filter = new PIXI.AbstractFilter(null, FragmentShader, {
+                displacementMap: {
+                    type: 'sampler2D',
+                    value: texture
+                },
+                scale: {
+                    type: '2f',
+                    value: { x: 15, y: 15 }
+                },
+                offset: {
+                    type: '2f',
+                    value: { x: 0,  y: 0 }
+                },
+                mapDimensions: {
+                    type: '2f',
+                    value: { x: 1,  y: 1 }
+                },
+                dimensions: {
+                    type: '4f',
+                    value: [0, 0, 0, 0]
+                },
+                LightPos: {
+                    type: '3f',
+                    value: [0, 1, 0]
+                }
+            });
+        }
+    });
+
+    return LightingFilter;
+
+});
