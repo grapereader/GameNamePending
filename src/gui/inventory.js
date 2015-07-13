a25458d2a3ac67d622a01c79a5527784406b5008
WinMac32 <seth@viaware.ca>
2015-07-13 15:49:22 -0600

    Make everything use the new sexy boxes

--- src/gui/inventory.js
+++ src/gui/inventory.js
@@ -1,61 +1,4 @@
-define(["util/helpers", "gui/windowobject"], function(Helpers, WindowObject) {
-
-    var Box = Class(WindowObject, {
-        constructor: function(gameManager, x, y, width, height) {
-            Box.$super.call(this, gameManager);
-
-            var g = new PIXI.Graphics();
-            g.lineStyle(2, 0x0, 1);
-
-            g.moveTo(5, 2);
-            g.lineTo(width - 4, 2);
-
-            g.moveTo(width - 4, 4);
-            g.lineTo(width - 2, 4);
-
-            g.moveTo(width - 1, 5);
-            g.lineTo(width - 1, height - 4);
-
-            g.moveTo(width - 4, height - 3);
-            g.lineTo(width - 2, height - 3);
-
-            g.moveTo(width - 4, height - 1);
-            g.lineTo(5, height - 1);
-
-            g.moveTo(3, height - 3);
-            g.lineTo(5, height - 3);
-
-            g.moveTo(2, height - 4);
-            g.lineTo(2, 5);
-
-            g.moveTo(3, 4);
-            g.lineTo(5, 4);
-
-            g.lineStyle(2, 0xF2C960, 1);
-
-            g.moveTo(5, 4);
-            g.lineTo(width - 4, 4);
-
-            g.moveTo(width - 3, 5);
-            g.lineTo(width - 3, height - 4);
-
-            g.moveTo(width - 4, height - 3);
-            g.lineTo(5, height - 3);
-
-            g.moveTo(4, height - 4);
-            g.lineTo(4, 5);
-
-            g.lineStyle(0, 0, 0);
-            g.beginFill(0x8E7348, 1);
-            g.drawRect(5, 5, width - 9, height - 9);
-            g.endFill();
-
-
-            this.addChild(g);
-
-            this.setPosition(x, y);
-        }
-    });
+define(["util/helpers", "gui/windowobject", "gui/box"], function(Helpers, WindowObject, Box) {
 
     var PADDING = 4;
     var SIZE = 50;
@@ -151,6 +94,8 @@ define(["util/helpers", "gui/windowobject"], function(Helpers, WindowObject) {
             background.endFill();
             this.container.addChild(background);
             */
+
+            this.addChild(new Box(gameManager, 0, 0, 300, 32 * this.height));
 
             for (var i = 0; i < inventory.items.length; i++) {
                 if (inventory.items[i] === false) continue;
--- src/gui/window.js
+++ src/gui/window.js
@@ -1,4 +1,4 @@
-define(["util/helpers", "gui/windowobject"], function(Helpers, WindowObject) {
+define(["util/helpers", "gui/windowobject", "gui/box"], function(Helpers, WindowObject, Box) {
 
     var Window = Class(WindowObject, {
         constructor: function(gameManager, width, height, titleString) {
@@ -7,17 +7,25 @@ define(["util/helpers", "gui/windowobject"], function(Helpers, WindowObject) {
             this.content = new WindowObject(gameManager);
             var title = new WindowObject(gameManager);
 
-            var titleGraphics = new PIXI.Graphics();
-            title.addChild(titleGraphics);
+            //var titleGraphics = new PIXI.Graphics();
+            //title.addChild(titleGraphics);
 
             var titleText = new PIXI.Text(titleString, {
                 font: Helpers.getFont(16)
             });
+
+            var padY = 6;
+
+            titleText.x = 6;
+            titleText.y = padY / 2;
+
+            title.addChild(new Box(gameManager, 0, 0, width, titleText.height + padY));
             title.addChild(titleText);
+            /*
             titleGraphics.beginFill(0xFFFFFF, 0.8);
             titleGraphics.drawRect(0, 0, width, titleText.height);
             titleGraphics.endFill();
-
+*/
             Window.$superp.addChild.call(this, this.content);
             Window.$superp.addChild.call(this, title);
 
@@ -25,14 +33,16 @@ define(["util/helpers", "gui/windowobject"], function(Helpers, WindowObject) {
 
             var self = this;
             var down = false;
-            var offset = 0;
+            var offsetX = 0;
+            var offsetY = 0;
             title.container.interactive = true;
             title.container.on("mousedown", function(e) {
                 if (self.windowSystem !== undefined) {
                     self.windowSystem.requestFocus(self);
                 }
                 down = true;
-                offset = e.data.global.x - self.container.x;
+                offsetX = e.data.global.x - self.container.x;
+                offsetY = e.data.global.y - self.container.y;
             });
             title.container.on("mouseup", function() {
                 down = false;
@@ -40,8 +50,8 @@ define(["util/helpers", "gui/windowobject"], function(Helpers, WindowObject) {
 
             title.container.on("mousemove", function(e) {
                 if (down) {
-                    self.container.x = e.data.global.x - offset;
-                    self.container.y = e.data.global.y - 2;
+                    self.container.x = e.data.global.x - offsetX;
+                    self.container.y = e.data.global.y - offsetY;
                 }
             });
         },
